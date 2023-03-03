17172ea77776e069c4b3678d3e41f71d4f575580
ezmyrian <josh.ray1284@gmail.com>
2023-03-03 06:25:07 -0600

    Create secret code generator

--- board.rb
+++ board.rb
@@ -2,4 +2,19 @@ class Board
   def initialize
     @game_board = Array.new(12) { Array.new(4) }
   end
+
+  def secret_code
+    colors = ['Red', 'Blue', 'Green', 'Yellow', 'Orange', 'Black']
+    puts "Are blanks allowed? (y/n)"
+    blanks = gets.chomp.downcase
+    until blanks == 'y' || blanks == 'n'
+      puts "Answer must by y for yes or n for no"
+    end
+
+    if blanks == 'y'
+      colors << 'Blank'
+    end
+
+    secret_code = 4.times.map { colors.sample }
+  end
 end
