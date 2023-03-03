372745e0fb27c97509bf92b2caa03cbb3dcf31fa
ezmyrian <josh.ray1284@gmail.com>
2023-03-03 06:27:27 -0600

    Define play_game method

---
+++ main.rb
@@ -0,0 +1,20 @@
+# Game
+# Board
+# 12 guesses
+# No blanks
+# Duplicate colors allowed
+# One player vs pc
+# 4 digit code with 6 color choices
+# "Key pegs"
+# AI picks secret code
+# Provide feedback on player guess
+
+# Allow blanks after refactor
+
+require_relative 'game.rb'
+
+def play_game
+  game = Game.new
+end
+
+play_game()
