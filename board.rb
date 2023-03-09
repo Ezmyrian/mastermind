654908cde6d310f6951554ed79c5397d84d38b4a
ezmyrian <josh.ray1284@gmail.com>
2023-03-09 05:17:03 -0600

    Separate color generator from secret code and add announce_color method

--- board.rb
+++ board.rb
@@ -3,18 +3,24 @@ class Board
     @game_board = Array.new(12) { Array.new(4) }
   end
 
-  def secret_code
-    colors = ['Red', 'Blue', 'Green', 'Yellow', 'Orange', 'Black']
+  def code_colors
+    @colors = ['Red', 'Blue', 'Green', 'Yellow', 'Orange', 'Black']
     puts "Are blanks allowed? (y/n)"
     blanks = gets.chomp.downcase
     until blanks == 'y' || blanks == 'n'
       puts "Answer must by y for yes or n for no"
     end
-
     if blanks == 'y'
-      colors << 'Blank'
+      @colors << 'Blank'
     end
+    @colors
+  end
 
-    secret_code = 4.times.map { colors.sample }
+  def announce_colors
+    puts "The available colors are #{@colors}"
+  end
+
+  def secret_code
+    secret_code = 4.times.map { @colors.sample }
   end
 end
